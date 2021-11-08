1f57662a3474aba02a0e6a6b326395e91ac2ff87
Alexander Nookinov <anookinov@gmail.com>
2021-11-08 19:24:45 +0700

    add bevy ecs components and systems

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,38 @@
 use bevy::prelude::*;
 
+struct Person;
+
+struct Name(String);
+
 fn main() {
-    App::build().run();
+    App::build()
+        .add_startup_system(add_people.system())
+        .add_system(hello_world.system())
+        .add_system(greet_people.system())
+        .run();
+}
+
+fn hello_world() {
+    println!("hello world!");
+}
+
+fn add_people(mut commands: Commands) {
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Elaina Proctor".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Renzo Hume".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Zayna Nieves".to_string()));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("hello {}!", name.0);
+    }
 }
