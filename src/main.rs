47e683caec00ead79c9068424abe979b5f90724d
Alexander Nookinov <anookinov@gmail.com>
2021-11-08 19:38:21 +0700

    add Time and Timer resources

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,15 @@ use bevy::prelude::*;
 
 pub struct HelloPlugin;
 
+struct GreetTimer(Timer);
+
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut AppBuilder) {
         // add things to your app here
-        app.add_startup_system(add_people.system())
-            .add_system(hello_world.system())
+        // the reason we call from_seconds with the true flag is to make the timer repeat itself
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people.system())
+            // .add_system(hello_world.system())
             .add_system(greet_people.system());
     }
 }
@@ -26,9 +30,9 @@ fn main() {
         .run();
 }
 
-fn hello_world() {
-    println!("hello world!");
-}
+// fn hello_world() {
+//     println!("hello world!");
+// }
 
 fn add_people(mut commands: Commands) {
     commands
@@ -45,8 +49,12 @@ fn add_people(mut commands: Commands) {
         .insert(Name("Zayna Nieves".to_string()));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("hello {}!", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    // update our timer with the time elapsed since the last update
+    // if that caused the timer to finish, we say hello to everyone
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("hello {}!", name.0);
+        }
     }
 }
